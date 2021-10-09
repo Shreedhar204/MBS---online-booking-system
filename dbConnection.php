7ef75d8ba7a83c1582d1bb3f6b52eba8ef664e9e
Shreedhar204 <shreedhar.maharaj86@gmail.com>
2021-10-09 16:22:32 +0200

    registration backend
    registration screen working

--- dbConnection.php
+++ dbConnection.php
@@ -1,12 +1,22 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "mbsdb");
+$firstName=$_POST['firstName'];
+$surname=$_POST['surname'];
+$emailAddress=$_POST['emailAddress'];
+$password=$_POST['password'];
 
-if(!$conn){
-    die("Connection failed");
+//Database connection
+$conn = new mysqli("localhost", "root", "", "mbsdb");
+if ($conn->connect_error) {
+    die('Connection failed :'.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into mbstbl(firstName, surname, emailAddress, password)
+    values(?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$firstName, $surname, $emailAddress, $password);
+    $stmt->execute();
+    echo"registration successful";
+    $stmt->close();
+    $conn->close();
 }
-
-
-
 
 
 ?>
