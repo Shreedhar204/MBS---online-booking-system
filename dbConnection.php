394f324681046023c7224a78a7268b2267eec16a
Shreedhar204 <shreedhar.maharaj86@gmail.com>
2021-10-09 15:25:04 +0200

    Some backend
    A few tweaks to the index html
    Login php : done and working

---
+++ dbConnection.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "mbsdb");
+
+if(!$conn){
+    die("Connection failed");
+}
+
+
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+    $emailAddress = $_POST["emailAddress"];
+    $password = $_POST["password"];
+
+   
+
+    //Database connection
+$con = new mysqli("localhost", "root", "", "mbsdb");
+if ($con ->connect_error) {
+    die("Failed to connect : ".$con->connect_error);
+}else{
+    $stmt = $con->prepare("select * from mbstbl where emailAddress = ?");
+    $stmt->bind_param("s", $emailAddress);
+    $stmt ->execute();
+    $stmt_result = $stmt->get_result();
+    if($stmt_result->num_rows > 0){
+        $data = $stmt_result->fetch_assoc();
+        if($data['password'] === $password){
+            echo"Login succussfully";
+        }else{
+            echo"Invalid email or password";
+        }
+    }else{
+        echo "Invalid email or password";
+    }
+}
+
+
+?>
+
+
+<!-- include"dbConnection.php";
+
+if(isset($_POST["email"], $_POST["password"])){
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $query = mysqli_query($conn,"SELECT EmailAddress, Password FROM mbstbl WHERE email = '".$email."' AND password = '".$password."' ");
+
+    // if ($query || mysqli_num_rows($query) == 0) {
+    if (mysqli_num_rows($query)>0){
+       $_SESSION["name"] = 
+        
+    }
+    else{
+        echo"user does not exist";
+    }
+
+} -->
