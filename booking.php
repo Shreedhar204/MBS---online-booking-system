f6ee7ec5286a35d8f43e8ddcfce46844e14dda6e
Shreedhar204 <shreedhar.maharaj86@gmail.com>
2021-10-10 20:13:57 +0200

    booking screen fucntional
    booking html -  daily trips where deleted. User has one screen to make all trips
    
    Bookings successfully posting to databse

--- booking.php
+++ booking.php
@@ -6,4 +6,25 @@ if(!isset($_SESSION['username'])){
 }else{
     echo"You are logged in";
 }
+
+$pickUp=$_POST['pickUp'];
+$dropOff=$_POST['dropOff'];
+$date=$_POST['date'];
+$time=$_POST['time'];
+$username = $_SESSION['username'];
+//Database connection
+$conn = new mysqli("localhost", "root", "", "mbsdb");
+if ($conn->connect_error) {
+    die('Connection failed :'.$conn->connect_error);
+}
+
+$sql = "update mbstbl set pickUp='$pickUp', dropOff='$dropOff', date='$date', time='$time' where emailAddress='$username'";
+    if ($conn->query($sql) === TRUE) {
+        echo"Booking successful";
+    }else{
+        echo "Error: ".$sql."<br>".$conn->error;
+    }
+
+    $conn->close();
+
 ?>
